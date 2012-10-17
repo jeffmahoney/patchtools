c419a353faa570d38796e25005e9d93af4ddc1e8
Jeff Mahoney <jeffm@suse.com>
2012-10-17 14:26:46 -0400

    Made mainline repository config optional
    git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6.git will
    always be the official upstream repo and since we check to see if the
    remote associated with any repo points to that, we can identify most
    mainline repo clones. The option is still available for use with sites
    that have one clone they pull from git.kernel.org and others clone from that.

--- patch/Repos.py
+++ patch/Repos.py
@@ -5,15 +5,22 @@ Represent Git Repos
 """
 
 import os
-from ConfigParser import ConfigParser
+from ConfigParser import ConfigParser, NoOptionError
 
+MAINLINE_URL = """git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6.git"""
 
 class Repos:
     def __init__(self):
         config = ConfigParser()
-        config.read(['/etc/patch.cfg', os.path.expanduser('~/.patch.cfg'), './patch.cfg'])
+        config.read(['/etc/patch.cfg', os.path.expanduser('~/.patch.cfg'),
+                     './patch.cfg'])
         self.repos = config.get('repositories', 'search').split()
-        self.mainline_repos = config.get('repositories', 'mainline').split()
+        self.mainline_repos = [ MAINLINE_URL ]
+        try:
+            repos = config.get('repositories', 'mainline').split()
+            self.mainline_repos += repos
+        except NoOptionError, e:
+            pass
 
     def _canonicalize(self, path):
         if path[0] == '/':
@@ -31,6 +38,3 @@ class Repos:
 
     def get_default_mainline_repo(self):
         return self._canonicalize(self.mainline_repos[0])
-
-
-

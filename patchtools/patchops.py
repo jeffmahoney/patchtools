jeffmahoney/patchtools

commit e48fae0c81644bea351a837f5894960eac08b4a1

patchtools: use subprocess.run instead of subprocess.Popen
Python 3.5 introduced subprocess.run as an easier way to handle
subprocess communication.  Popen() stopped accepting the 'text'
argument so it's a good time to switch to that API.  Also added
a helper so running a command and capturing output is as simple
as run_command(command).

Fixes the following exception from Popen():
TypeError: __init__() got an unexpected keyword argument 'text'

Signed-off-by: Jeff Mahoney <jeffm@suse.com>

patchtools/patchops.py
@@ -4,7 +4,7 @@ Support package for doing SUSE Patch operations
 """
 
 from patchtools import PatchException
-from subprocess import Popen, PIPE
+from patchtools.command import run_command
 import re
 
 def key_version(tag):
@@ -39,12 +39,8 @@ class LocalCommitException(PatchException):
 class PatchOps:
     @staticmethod
     def get_tag(commit, repo):
-        command = "(cd %s;git name-rev --refs=refs/tags/v* %s)" % (repo, commit)
-
-        cmd = Popen(command, shell=True, text=True,
-                    stdout=PIPE, stderr=open("/dev/null", "w"))
-        tag = cmd.communicate()[0]
-
+        command = f"(cd {repo};git name-rev --refs=refs/tags/v* {commit})"
+        tag = run_command(command)
         if tag == "":
             return None
 
@@ -58,11 +54,8 @@ class PatchOps:
 
     @staticmethod
     def get_next_tag(repo):
-        command = "(cd %s ; git tag -l 'v*')" % repo
-        cmd = Popen(command, shell=True, text=True,
-                    stdout=PIPE, stderr=open("/dev/null", "w"))
-        tag = cmd.communicate()[0]
-
+        command = f"(cd {repo} ; git tag -l 'v*')"
+        tag = run_command(command)
         if tag == "":
             return None
 
@@ -85,20 +78,13 @@ class PatchOps:
 
     @staticmethod
     def get_diffstat(message):
-
-        cmd = Popen("diffstat -p1", shell=True, text=True,
-                    stdin=PIPE, stdout=PIPE)
-
-        out = cmd.communicate(message)[0]
-
-        return out
+        return run_command("diffstat -p1", input=message)
 
     @staticmethod
     def get_git_repo_url(dir):
-        command = "(cd %s; git remote show origin -n)" % dir
-        cmd = Popen(command, shell=True, text=True,
-                    stdout=PIPE, stderr=open("/dev/null", "w"))
-        for line in cmd.communicate()[0].split('\n'):
+        command = f"(cd {dir}; git remote show origin -n)"
+        output = run_command(command)
+        for line in output:
             m = re.search("URL:\s+(\S+)", line)
             if m:
                 return m.group(1)
@@ -107,10 +93,8 @@ class PatchOps:
 
     @staticmethod
     def confirm_commit(commit, repo):
-        cmd = Popen("cd %s ; git rev-list HEAD --not --remotes $(git config --get branch.$(git symbolic-ref --short HEAD).remote)" % repo,
-                    shell=True, text=True,
-                    stdout=PIPE, stderr=open("/dev/null", "w"))
-        out = cmd.communicate()[0]
+        command = f"cd {repo} ; git rev-list HEAD --not --remotes $(git config --get branch.$(git symbolic-ref --short HEAD).remote)"
+        out = run_command(command)
         if out == "":
             return True
 
@@ -121,11 +105,8 @@ class PatchOps:
 
     @staticmethod
     def get_commit(commit, repo, force=False):
-        cmd = Popen("cd %s ; git diff-tree --no-renames --pretty=email -r -p --cc --stat %s" % \
-                    (repo, commit),
-                    shell=True, text=True,
-                    stdout=PIPE, stderr=open("/dev/null", "w"))
-        data = cmd.communicate()[0]
+        command = f"cd {repo}; git diff-tree --no-renames --pretty=email -r -p --cc --stat {commit}"
+        data = run_command(command)
         if data == "":
             return None
 

patchtools/command.py
@@ -1,0 +1,8 @@
+import subprocess
+import sys
+
+def run_command(command, stdin=None, input=None, stdout=subprocess.PIPE):
+    proc = subprocess.run(command, shell=True, encoding='utf-8',
+                          stdin=stdin, input=input, stdout=stdout,
+                          stderr=open("/dev/null", "w"))
+    return proc.stdout

patchtools/config.py
@@ -7,16 +7,16 @@ import os
 import pwd
 import site
 import configparser
-from subprocess import Popen, PIPE
 import re
+
+from patchtools.command import run_command
 
 MAINLINE_URLS = [ """git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6.git""", """git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git""" ]
 
 def get_git_repo_url(gitdir):
-    command = "(cd %s; git remote show origin -n)" % gitdir
-    cmd = Popen(command, shell=True, text=True,
-                stdout=PIPE, stderr=open("/dev/null", "w"))
-    for line in cmd.communicate()[0].split('\n'):
+    command = f"(cd {gitdir}; git remote show origin -n)"
+    output = run_command(command)
+    for line in output.split('\n'):
         m = re.search("URL:\s+(\S+)", line)
         if m:
             return m.group(1)
@@ -24,10 +24,8 @@ def get_git_repo_url(gitdir):
     return None
 
 def get_git_config(gitdir, var):
-    command = "(cd {}; git config {})".format(gitdir, var)
-    cmd = Popen(command, shell=True, text=True,
-                stdout=PIPE, stderr=open("/dev/null", "w"))
-    return cmd.communicate()[0].strip()
+    command = f"(cd {gitdir}; git config {var})"
+    return run_command(command).strip()
 
 # We deliberately don't catch exceptions when the option is mandatory
 class Config:

commit ac76d76128a98362de549644f68b92149fb1eff1

Fix parsing of git remotes
The output of `git remote show` must be explicitly split into
lines. Without this fix, `exportpatch` does not add the appropriate
`Patch-mainline` header.

Fixes: e48fae0c81644bea351a837f5894960eac08b4a1
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

patchtools/patchops.py
@@ -78,7 +78,7 @@ def get_diffstat(message):
 def get_git_repo_url(dir):
     command = f"(cd {dir}; git remote show origin -n)"
     output = run_command(command)
-    for line in output:
+    for line in output.split('\n'):
         m = re.search("URL:\s+(\S+)", line)
         if m:
             return m.group(1)